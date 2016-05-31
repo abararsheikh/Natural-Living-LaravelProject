022039f147bfafa5f4fcb6338c650cc98f2e40ad
abrar <er.abrar@gmail.com>
2016-05-31 11:15:39 -0400

    testing repository

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <p>Welcome to Natural-Living site</p>
             </div>
         </div>
     </body>

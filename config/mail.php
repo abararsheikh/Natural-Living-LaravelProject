717bbc692f305982a4e2cec4ab92cfcb4d690877
abrar <er.abrar@gmail.com>
2016-06-08 23:59:42 -0400

    password reset link has been added

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'abrar.phpproject@gmail.com', 'name' => 'Password Reset'],
 
     /*
     |--------------------------------------------------------------------------
